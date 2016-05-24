2cfc3783b6e15ac5e9c6ace7f2d5ec9e26bc16c4
Andrew Jarombek <ajarombek95@gmail.com>
2016-05-24 16:02:39 -0400

    V.0.0.2 - Some Basic CSS and Small Homepage Changes

--- views/home.php
+++ views/home.php
@@ -5,20 +5,25 @@ Main Signed Out Page HTML Code
 -->
 
     <body>
-        <header>
-            <h1>SaintsXCTF</h1>
+        <header id='signedoutmenu'>
+            <a href='index.php'><img id='sitelogo' src='views/images/logo.jpg' alt='logo'><label id='sitetitle'>SaintsXCTF</label></a>
         </header>
-        <div id=register>
-            <h2>Register</h2>
-            <input type='text' name='username' placeholder='Username'><br>
-            <input type='text' name='first' placeholder='First Name'><br>
-            <input type='text' name='last' placeholder='Last Name'><br>
-            <input type='password' name='password' placeholder='Password'><br>
-            <input type='password' name='confirmpassword' placeholder='Confirm Password'><br>
-        </div>
-        <div id=signin>
-            <h2>Sign In</h2>
-            <input type='text' name='username' placeholder='Username'><br>
-            <input type='password' name='password' placeholder='Password'><br>
+        <div id='mainbody'>
+            <h1>Saints XC Running Logs</h1>
+            <div id=register>
+                <h2>Register</h2>
+                <input id='input' type='text' name='username' placeholder='Username'><br>
+                <input id='input' type='text' name='first' placeholder='First Name'><br>
+                <input id='input' type='text' name='last' placeholder='Last Name'><br>
+                <input id='input' type='password' name='password' placeholder='Password'><br>
+                <input id='input' type='password' name='confirmpassword' placeholder='Confirm Password'><br>
+                <input id='submit' type='button' name='register' disabled='true' value='Register'><br>
+            </div>
+            <div id=signin>
+                <h2>Sign In</h2>
+                <input id='input' type='text' name='username' placeholder='Username'><br>
+                <input id='input' type='password' name='password' placeholder='Password'><br>
+                <input id='submit' type='button' name='signin' disabled='true' value='Sign In'><br>
+            </div>
         </div>
     </body>

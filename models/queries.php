2f261c05b9f1dea5838986e26b8a6865bce712d6
Andrew Jarombek <ajarombek95@gmail.com>
2016-05-28 11:20:00 -0400

    V.0.0.11 - ResetDB and Queries

---
+++ models/queries.php
@@ -0,0 +1,23 @@
+<?php
+
+// Class To Search the Database and Add to the Database
+class Queries {
+    
+    private $db; // PDO Construct
+    
+    function __construct($db) {
+        $this->db = $db;
+    }
+    
+    function usernameExists($username) {
+        
+    }
+    
+    function addUser($username, $first, $last, $password) {
+        
+    }
+    
+    function addGroups() {
+        
+    }
+}

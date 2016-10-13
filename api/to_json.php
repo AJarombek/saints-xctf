72495e6aae7c5f46ee9236c0fcc95a7b73563390
Andrew Jarombek <ajarombek95@gmail.com>
2016-10-12 22:01:24 -0400

    V.0.1.8 - Start MySQL Query to JSON conversion

---
+++ api/to_json.php
@@ -0,0 +1,13 @@
+<?php
+
+// Author: Andrew Jarombek
+// Date: 10/12/2016 - 
+// Convert arrays from database queries to JSON objects for the API
+
+namespace Rest;
+require_once('to_json.php');
+
+class ToJSON
+{
+	 
+}
--- api/user_rest_controller.php
+++ api/user_rest_controller.php
@@ -6,31 +6,42 @@
 
 namespace Rest;
 require_once('rest_controller.php');
+require_once('to_json.php');
 
 class UserRestController implements RestController
 {
 	private $user;
+	private $queries;
 
 	public function __construct($user = null)
 	{
 		$this->user = $user;
+		$to_json = new ToJSON();
 	}
 
+	// Get either a specific user or all the users
 	public function get($instance = null) 
 	{
-
+		if (isset($instance)) {
+			// Get a specific users information
+		} else {
+			// Get all users information
+		}
 	}
 
+	// Add a user to the api
 	public function post($instance = null) 
 	{
 		
 	}
 
+	// Update a specific user in the api
 	public function put($instance = null) 
 	{
 		
 	}
 
+	// Delete a specific user in the api
 	public function delete($instance = null) 
 	{
 		
